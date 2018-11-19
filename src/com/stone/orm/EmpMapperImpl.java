9ba72f48bb86d715a308b2cc56c6e92132006337
stonebox1122 <287779438@qq.com>
2018-11-19 15:06:22 +0800

    commit

--- src/com/stone/orm/EmpMapperImpl.java
+++ src/com/stone/orm/EmpMapperImpl.java
@@ -1,13 +1,6 @@
 package com.stone.orm;
-
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
-
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class EmpMapperImpl implements EmpMapper {
 	
@@ -28,26 +21,19 @@ public class EmpMapperImpl implements EmpMapper {
 	@Override
 	public int insert(Emp record) {
 		try {
-			//1、读取主配置文件
-			InputStream inputStream = Resources.getResourceAsStream("mybatisConfig.xml");
-			//2、创建SqlSessionFactory对象
-			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-			//3、创建SqlSession对象
-			SqlSession session = sqlSessionFactory.openSession();
-			//4、操作
+			//1、use MyBatiUtil get SqlSession instance
+			session = MyBatisUtil.getSqlSession();
+			//2、操作
 			session.insert("com.stone.orm.EmpMapper.insert", record);
-			//5、SqlSession提交
+			//3、SqlSession提交
 			session.commit();
 			return 1;
-		} catch (IOException e) {
-			e.printStackTrace();
 		} finally {
-			//6、SqlSession关闭
+			//4、SqlSession关闭
 			if (session != null) {
 				session.close();
 			}
 		}
-		return 0;
 	}
 
 	@Override
