923fb28433bc6b73b1ac19af47bbf9dc667c3cb2
stonebox1122 <287779438@qq.com>
2018-11-29 14:45:38 +0800

    commit

--- src/com/stone/test/EmpTest.java
+++ src/com/stone/test/EmpTest.java
@@ -10,7 +10,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.stone.dao.EmpMapper;
-import com.stone.entity.Departments;
 import com.stone.entity.Emp;
 import com.stone.util.MyBatisUtil;
 
@@ -49,7 +48,7 @@ public class EmpTest {
 	
 	@Test
 	public void test02() {
-		empMapper.deleteByEmpno(8900);
+		empMapper.deleteByEmpno(41);
 		sqlSession.commit();
 	}
 	
@@ -57,7 +56,7 @@ public class EmpTest {
 	public void test03() throws Exception {
 		Emp emp = new Emp();
 		emp.setEmpno(8899);
-		emp.setEname("ccc");
+		emp.setEname("eee");
 		emp.setHiredate(new java.sql.Date(date.getTime()));
 		emp.setJob("ddd");
 		emp.setMgr(7902);
@@ -97,166 +96,5 @@ public class EmpTest {
 		map.put("empno", 31);
 		emp = empMapper.selectByMap(map);
 		System.out.println(emp);
-	}
-	
-	@Test
-	public void test09() throws Exception {
-		Map<String,Object> map = new HashMap<String, Object>();
-		map.put("ename", "A");
-		map.put("sal", 2000);
-		List<Emp> emps = empMapper.selectEmpByMap(map);
-		for (Emp emp : emps) {
-			System.out.println(emp);
-		}
-	}
-	
-	@Test
-	public void test10() throws Exception {
-		List<Emp> emps = empMapper.selectEmpByConditions("A", 2000);
-		for (Emp emp : emps) {
-			System.out.println(emp);
-		}
-	}
-	
-	@Test
-	public void test11() throws Exception {
-		//条件是1=1
-		Emp e1 = new Emp();
-		List<Emp> emps1 = empMapper.selectEmpIf(e1);
-		for (Emp emp : emps1) {
-			System.out.println(emp);
-		}
-		//条件是ename like %A%
-		Emp e2 = new Emp();
-		e2.setEname("A");
-		List<Emp> emps2 = empMapper.selectEmpIf(e2);
-		for (Emp emp : emps2) {
-			System.out.println(emp);
-		}
-		//条件是sal>2000
-		Emp e3 = new Emp();
-		e3.setSal(2000.00);
-		List<Emp> emps3 = empMapper.selectEmpIf(e3);
-		for (Emp emp : emps3) {
-			System.out.println(emp);
-		}
-		//条件是ename like %A% and sal>2000
-		Emp e4 = new Emp();
-		e4.setEname("A");
-		e4.setSal(2000.00);
-		List<Emp> emps4 = empMapper.selectEmpIf(e4);
-		for (Emp emp : emps4) {
-			System.out.println(emp);
-		}
-	}
-	
-	@Test
-	public void test12() throws Exception {
-		//条件是1=1
-		Emp e1 = new Emp();
-		List<Emp> emps1 = empMapper.selectEmpWhere(e1);
-		for (Emp emp : emps1) {
-			System.out.println(emp);
-		}
-		//条件是ename like %A%
-		Emp e2 = new Emp();
-		e2.setEname("A");
-		List<Emp> emps2 = empMapper.selectEmpWhere(e2);
-		for (Emp emp : emps2) {
-			System.out.println(emp);
-		}
-		//条件是sal>2000
-		Emp e3 = new Emp();
-		e3.setSal(2000.00);
-		List<Emp> emps3 = empMapper.selectEmpWhere(e3);
-		for (Emp emp : emps3) {
-			System.out.println(emp);
-		}
-		//条件是ename like %A% and sal>2000
-		Emp e4 = new Emp();
-		e4.setEname("A");
-		e4.setSal(2000.00);
-		List<Emp> emps4 = empMapper.selectEmpWhere(e4);
-		for (Emp emp : emps4) {
-			System.out.println(emp);
-		}
-	}
-	
-	@Test
-	public void test13() throws Exception {
-		//条件是1=0,没有返回结果
-		Emp e1 = new Emp();
-		List<Emp> emps1 = empMapper.selectEmpChoose(e1);
-		for (Emp emp : emps1) {
-			System.out.println(emp);
-		}
-		//条件是ename like %A%,有返回结果
-		Emp e2 = new Emp();
-		e2.setEname("A");
-		List<Emp> emps2 = empMapper.selectEmpChoose(e2);
-		for (Emp emp : emps2) {
-			System.out.println(emp);
-		}
-		//条件是sal>2000,有返回结果
-		Emp e3 = new Emp();
-		e3.setSal(2000.00);
-		List<Emp> emps3 = empMapper.selectEmpChoose(e3);
-		for (Emp emp : emps3) {
-			System.out.println(emp);
-		}
-		//条件是ename like %A% and sal>2000，加上了2个条件反而没有结果
-		Emp e4 = new Emp();
-		e4.setEname("A");
-		e4.setSal(2000.00);
-		List<Emp> emps4 = empMapper.selectEmpChoose(e4);
-		for (Emp emp : emps4) {
-			System.out.println(emp);
-		}
-	}
-	
-	@Test
-	public void test14() throws Exception {
-		Object[] empnos = new Object[] {7499,7521};
-		List<Emp> emps = empMapper.selectEmpForeachArray(empnos);
-		System.out.println(emps);
-	}
-	
-	@Test
-	public void test15() throws Exception {
-		List<Integer> empnos = new ArrayList<Integer>();
-		empnos.add(7499);
-		empnos.add(7521);
-		List<Emp> emps = empMapper.selectEmpForeachList(empnos);
-		System.out.println(emps);
-	}
-	
-	@Test
-	public void test16() throws Exception {
-		Emp emp1 = new Emp();
-		emp1.setEmpno(7499);
-		Emp emp2 = new Emp();
-		emp2.setEmpno(7521);
-		
-		List<Emp> emps = new ArrayList<Emp>();
-		emps.add(emp1);
-		emps.add(emp2);
-		
-		emps = empMapper.selectEmpForeachList2(emps);
-		System.out.println(emps);
-	}
-	
-	@Test
-	public void test17() throws Exception {
-		Emp emp1 = new Emp();
-		emp1.setEmpno(7499);
-		Emp emp2 = new Emp();
-		emp2.setEmpno(7521);
-		
-		List<Emp> emps = new ArrayList<Emp>();
-		emps.add(emp1);
-		emps.add(emp2);
-		
-		emps = empMapper.selectEmpBySQLFragment(emps);
-		System.out.println(emps);
 	}
 }
